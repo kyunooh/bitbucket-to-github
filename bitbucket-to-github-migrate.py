4dcfa4a013747c506754400418ae75db1ad6c830
Hyeon-Mook Jerry Choi <chm073@gmail.com>
2018-01-26 22:26:57 +0900

    Clone bitbucket repositries to local

--- bitbucket-to-github-migrate.py
+++ bitbucket-to-github-migrate.py
@@ -2,13 +2,35 @@ import os
 import json
 
 import requests
+import git
 
+
+bitbucket_base_url = 'https://api.bitbucket.org/'
 
 bitbucket_username = os.getenv('BITBUCKET_USERNAME', '')
 bitbucket_password = os.getenv('BITBUCKET_PASSWORD', '')
-r = requests.get('https://api.bitbucket.org/2.0/repositories/' + bitbucket_username, auth=(bitbucket_username, bitbucket_password))
+my_repository_response = requests.get(
+    bitbucket_base_url + '2.0/repositories/' + bitbucket_username,
+    auth=(bitbucket_username, bitbucket_password))
 
-bitbuckets = json.loads(r.text)
+
+my_repositories = json.loads(my_repository_response.text)['values']
+
+repository_base_dir = './repositories'
+
+if not os.path.exists(repository_base_dir):
+    os.makedirs(repository_base_dir)
+
+for repository in my_repositories:
+    repository_dir = repository_base_dir
+    if not os.path.exists(repository_dir):
+        os.makedirs(repository_dir)
+
+    git.Git(repository_dir).clone(repository['links']['clone'][0]['href'])
 
 
-print(bitbuckets)
+
+
+
+
+
